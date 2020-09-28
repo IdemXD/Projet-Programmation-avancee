75b1901bdc7302f708b523d06d6498749d57d425
Chloe-Ms <mllechloe.mathias@gmail.com>
2020-09-28 20:25:39 +0200

    v0.8 Renommage et ajout des sprites joueurs

--- personnages.c
+++ personnages.c
@@ -1,10 +1,10 @@
 /**
-	*\file characters.c
+	*\file personnages.c
 	*\author Chloe Mathias
 	*\version 1.0
 */
 
-#include "character.h"
+#include "personnages.h"
 #include "constantes.h"
 
 
@@ -12,11 +12,16 @@
  {
  	character_s* player = malloc (sizeof(character_s)*NB_PERSONNAGES); //les personnages du joueur
  	for (int i=0; i<NB_PERSONNAGES;i++){
- 		player[i].coord_x = 4;/:On place les personnages dans la salle de départ (au centre)
+ 		player[i].coord_x = 4;//On place les personnages dans la salle de départ (au centre)
  		player[i].coord_y = 4;
  		player[i].state = 1 ; //les personnages sont vivants
  		
  	}
  	return player ;
 
+ }
+
+ void free_characters( character_s*  ch)
+ {
+ 	free(player);
  }
--- personnages.h
+++ personnages.h
@@ -1,5 +1,5 @@
 /**
-	*\file characters.h
+	*\file personnages.h
 	*\author Chloe Mathias
 	*\version 1.0
 */
@@ -7,10 +7,16 @@
 struct character_t {
 	int coord_x; /*!< Tableau contenant la coordonnée x du personnage */
 	int coord_y; /*!< Tableau contenant la coordonnée y du personnage */
-	int state; /*!< Etat du personnage(4 quand le joueur est vivant)*/
+	int state; /*!< Etat du personnage(1 quand le joueur est vivant)*/
 }
 
 typedef struct character_t character_s;
 
 
+/**
+	*\brief alloue l'espace et initialise les personnages
+	*\return tableau des struct de joueurs
+*/
+
+character_s*  create_characters();
 
