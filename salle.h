322fb74fa424c6876c5b5e3fbee022164f53d0e0
IdemXD <medi.boudhanee@laposte.net>
2020-09-28 22:56:59 +0200

    v0.9

--- salle.h
+++ salle.h
@@ -1,13 +1,23 @@
 #ifndef Salle_h
 #define Salle_h
 
-#include "Plateau.h"
+#include "plateau.h"
 
 struct Salle
 {
-    int i;
-    int j;
-    int etat;
-    char S,R,E,V,D,X,T,C,F,M,O,N;
+    int x;      /*!< Tableau contenant la coordonnée x de la salle */
+    int y;      /*!< Tableau contenant la coordonnée y de la salle */
+    int etat;   /*!< Etat de la salle(1 si la salle est utilisable )*/
+    char S;     
+    char R;
+    char E;
+    char V;
+    char X;
+    char T;
+    char C; 
+    char F;
+    char M;
+    char O;
+    char N;
 };
 typedef struct Salle Salle_t;
