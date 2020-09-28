ef1befb25595de83822b22a544d0e681bb84fc8d
Chloe-Ms <mllechloe.mathias@gmail.com>
2020-09-28 19:33:52 +0200

    v0.8 ajout des constantes de l'ecran

--- constantes.h
+++ constantes.h
@@ -4,8 +4,20 @@
 	*\version 1.0
 */
 
+/**
+ * \brief Nombre de personnages joués
+ */
 #define NB_PERSONNAGES 2
 
 
+/**
+ * \brief Hauteur de l'écran de jeu
+ */
+#define SCREEN_HEIGHT 600
 
 
+/**
+ * \brief Largeur de l'écran de jeu
+ */
+
+#define SCREEN_WIDTH 900
