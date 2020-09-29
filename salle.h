85b2343d6d0ba848529c5e8a261298d587d5ca70
IdemXD <medi.boudhanee@laposte.net>
2020-09-29 22:47:33 +0200

    v0.9

--- salle.h
+++ salle.h
@@ -7,17 +7,7 @@ struct Salle
 {
     int x;      /*!< Tableau contenant la coordonnée x de la salle */
     int y;      /*!< Tableau contenant la coordonnée y de la salle */
-    int etat;   /*!< Etat de la salle(1 si la salle est utilisable )*/
-    char S;     
-    char R;
-    char E;
-    char V;
-    char X;
-    char T;
-    char C; 
-    char F;
-    char M;
-    char O;
-    char N;
+    int state;   /*!< Etat de la salle(1 si la salle est utilisable )*/
+    char S;
 };
 typedef struct Salle Salle_t;
