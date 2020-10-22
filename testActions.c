26a2f575cd5091fa6ecd6def84ca1e0e427e9268
Chloe-Ms <mllechloe.mathias@gmail.com>
2020-10-22 15:46:20 +0200

    v1.13 Correction actions.c et salle.c

---
+++ testActions.c
@@ -0,0 +1,23 @@
+/**
+	*\file testActions.c
+	*\author Chloe Mathias
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "actions.h"
+#include "plateau.h"
+
+int main (int argv,char* argv[]){
+
+	salle ** pl;
+	persos_s* p;
+
+	pl = creer_plateau();
+	p = creer_persos();
+
+	
+
+	return 0;
+}
