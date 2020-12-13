8e539a6c93131868dcd16e5f8eea8b566401c29e
Mandragor54 <marie.trunkenwald@gmail.com>
2020-12-13 14:40:40 +0100

    v?.? Version non fonctionelle de l'ajout du struct world

---
+++ data_monde.h
@@ -0,0 +1,61 @@
+#ifndef MONDE_H
+#define MONDE_H
+
+/**
+  * \file data_monde.h
+  * \author Trunkenwald Marie
+  * \brief Rassemble les éléments du jeu
+*/
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+#include "actions.h"
+#include "fonctions_SDL.h"
+#include "personnages.h"
+#include "plateau.h"
+
+struct data_s {
+    int terminer;           /*!< Indique que la partie est terminée */
+    int etape;              /*!< Indique à quel étape du jeu on se trouve */
+    int trouve;             /*!< Indique qu'on a trouvé la salle 25 */
+    int tour_perso;         /*!< Indique à quel joueur c'est le tour */
+    int tour_action;        /*!< Indique ordre d'execution des actions  */
+    int nb_action;          /*!< Donne le nb d'action accordé au joueur */
+    char active_direction;  /*!< Définit quel direction le joueur choisit */
+
+    salle_t** salles;       /*!< Represente le plateau de jeu */
+    persos_t* joueur;        /*!< Represente les joueurs */
+    action_t* actions;      /*!< Organise les actions des joueurs */
+};
+
+/**
+    * \brief Type qui correspond aux données du jeu
+*/
+
+typedef struct data_s data_t;
+
+/**
+    * \brief Initialisation des données du jeu
+    * \param data les éléments du joueur
+*/
+void init_data(data_t* data);
+
+/**
+    * \brief Rafraichis l'affichage du jeu
+    * \param ecran l'ecran
+    * \param textures les textures
+    * \param data les elements
+*/
+void refresh_game(SDL_Renderer *ecran, ressources textures, data_t* data);
+
+/**
+    * \brief Nettoie le jeu, ses éléments et libère la mémoire
+    * \param fenetre la fenêtre du jeu
+    * \param ecran l'ecran
+    * \param textures les textures
+    * \param data les elements
+*/
+void clean_game(SDL_Window *fenetre, SDL_Renderer *ecran, ressources *textures, data_t* data);
+
+#endif
